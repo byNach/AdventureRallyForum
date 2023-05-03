33eaadc0047bfa7f7e91856e3802884506a1d372
nach <nachnitror@gmail.com>
2023-05-03 12:17:57 +0200

    CREATE header

--- AdventureRallyForum/src/Components/Header.jsx
+++ AdventureRallyForum/src/Components/Header.jsx
@@ -1,7 +1,32 @@
+import "../assets/Styles/Header-style.css";
+import helmet from "../assets/Images/icons8-helmet-64.png";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
-    <h1>Header</h1>
-  )
-}
+    <div className="Header">
+      <div className="Header-box">
+        <div className="Header-base">
+          <div className="Name-logo-box">
+            AdventureRallyForum
+            <img className="Helmet-img" src={helmet}></img>
+          </div>
+          <div className="Header-credentials">
+            <Link to="/Forum">
+              <button className="Foro-button">FORO</button>
+            </Link>
+            <Link to="/Events">
+              <button className="Eventos-button">EVENTOS</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="Nav-down">
+        <div className="Sing-up">Sing Up</div>
+        <div className="Login">Login</div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
