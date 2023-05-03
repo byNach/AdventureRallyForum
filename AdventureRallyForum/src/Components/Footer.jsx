bbca5329e2335744175263b252bf98a9d63406a5
nach <nachnitror@gmail.com>
2023-05-03 09:23:20 +0200

    DONE footer v1

--- AdventureRallyForum/src/Components/Footer.jsx
+++ AdventureRallyForum/src/Components/Footer.jsx
@@ -1,25 +1,38 @@
 import "../assets/Styles/Footer-style.css";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 const Footer = () => {
   return (
-    <div className="Footer-base">
-      <div className="Title-box">
-        <h1 className="Footer-title ">AdventureRallyForum.com</h1>
+    <div className="Footer">
+      <div className="Footer-top">
+        <div className="Footer-title ">AdventureRallyForum.com</div>
       </div>
-      <div className="Developer-box">
-        <p>2023 devByNach</p>
-      </div>
-      <div className="Breadcrumbs-box">
-        <Breadcrumbs aria-label="breadcrumb">
-          <Link underline="hover" color="white" href="/">
-            Normas
-          </Link>
-          <Link underline="hover" color="white" href="/">
-            Contacto
-          </Link>
-        </Breadcrumbs>
+      <div className="Footer-box">
+        <div className="Footer-base">
+          <div className="Breadcrumbs-box">
+            <Breadcrumbs aria-label="breadcrumb">
+              <Link underline="hover" color="white" href="/">
+                Normas
+              </Link>
+              <Link underline="hover" color="white" href="/">
+                Contacto
+              </Link>
+              <Link underline="hover" color="white" href="/">
+                Sobre nosotros
+              </Link>
+            </Breadcrumbs>
+          </div>
+          <div className="Developer-box">
+            <p>Copyright ©2023 ByNach</p>
+          </div>
+            <div className="Social-box">
+              <LinkedInIcon color="white" fontSize="large" />
+              <InstagramIcon color="white" fontSize="large" />
+            </div>
+        </div>
       </div>
     </div>
   );
