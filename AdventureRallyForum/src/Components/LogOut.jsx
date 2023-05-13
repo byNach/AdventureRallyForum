b3bfa184a956d22680864185e8056054148013c2
nach <nachnitror@gmail.com>
2023-05-13 12:18:51 +0200

    CREATE logOut.jsx

---
+++ AdventureRallyForum/src/Components/LogOut.jsx
@@ -0,0 +1,36 @@
+import { useAuth } from "../Context/AuthContext";
+import { getAuth, signOut } from "firebase/auth";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import "../assets/Styles/Styled-components/LogOut-style.css";
+
+const LogOut = () => {
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  const handleLogOut = (e) => {
+    e.preventDefault();
+    signOut(auth)
+      .then(() => {
+        console.log("Sign-out successful.");
+        navigate("/Home");
+      })
+      .catch((error) => {
+        console.log("An error happened.");
+      });
+  };
+
+  return (
+    <form className="Login-base">
+      <div className="Login-title">Log Out</div>
+      <div className="Inputs-base">Pulsa LOGOUT para cerrar tu sesión.</div>
+      <div className="Inputs-base">
+        Aunque recuerda, cada vez que te vas, una KTM va al taller.
+      </div>
+      <button className="Submit-button" onClick={(e) => handleLogOut(e)}>
+        Log Out
+      </button>
+    </form>
+  );
+};
+
+export default LogOut;
