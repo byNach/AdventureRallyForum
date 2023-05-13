9471f9a1eddd334a3978136a6de95364156df7c1
nach <nachnitror@gmail.com>
2023-05-13 12:57:42 +0200

    ADD redirect to Home after user login

--- AdventureRallyForum/src/Components/Login.jsx
+++ AdventureRallyForum/src/Components/Login.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import { useAuth } from "../Context/AuthContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../assets/Styles/Styled-components/Login-style.css";
 
 const Login = () => {
   const auth = useAuth();
-  console.log(auth.user.email);
+  const navigate = useNavigate(); // add this line
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    auth.login(email, password);
-  };
+    await auth.login(email, password); // wait for the login to complete
+    navigate('/Home'); // navigate programmatically
+  }
 
   const handleGoogle = (e) => {
     e.preventDefault;
