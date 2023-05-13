f38870ab11c8a610190301f8227b316baafd6870
nach <nachnitror@gmail.com>
2023-05-13 12:08:32 +0200

    ADD LogOutPage and redirect inexistent page to "/"

--- AdventureRallyForum/src/Routing.jsx
+++ AdventureRallyForum/src/Routing.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./Pages/Home";
 import Forum from "./Pages/Forum";
 import Events from "./Pages/Events";
 import LogInPage from "./Pages/LogInPage";
 import SignUpPage from "./Pages/SignUpPage";
+import LogOutPage from "./Pages/LogOutPage";
 import EventDetails from "./Pages/EventDetails";
 import InfoPage from "./Pages/InfoPage";
 
@@ -19,7 +20,9 @@ const Routing = () => {
           <Route path="/EventDetails" element={<EventDetails />} />
           <Route path="/LogInPage" element={<LogInPage />} />
           <Route path="/SignUpPage" element={<SignUpPage />} />
+          <Route path="/LogOutPage" element={<LogOutPage />} />
           <Route path="/Info" element={<InfoPage />} />
+          <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </BrowserRouter>
   );
