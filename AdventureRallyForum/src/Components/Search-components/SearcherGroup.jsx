60035cd852a6bb735730925fa61d070671366577
nach <nachnitror@gmail.com>
2023-05-22 12:37:34 +0200

    IMPORT context and ADD button call function

--- AdventureRallyForum/src/Components/Search-components/SearcherGroup.jsx
+++ AdventureRallyForum/src/Components/Search-components/SearcherGroup.jsx
@@ -1,12 +1,19 @@
-import * as React from "react";
+import { useContext } from "react";
 import MonthList from "./Month";
 import CountryList from "./Country";
 import StateList from "./State";
 import VehicleList from "./Vehicle";
 import NavList from "./Nav";
+import { FilterContext } from "../../Context/FilterContext";
 import "../../assets/Styles/Styled-components/Searcher-style.css";
 
 const SearcherGroup = () => {
+  const { Prueba } = useContext(FilterContext);
+
+  const handleClick = () => {
+    Prueba();
+  };
+
   return (
     <div className="Group-base">
       <div className="Group-title">Buscador de eventos</div>
@@ -18,7 +25,9 @@ const SearcherGroup = () => {
         <NavList />
       </div>
       <div className="Filter-button-box">
-        <button className="Filter-button">Filtrar</button>
+        <button className="Filter-button" onClick={handleClick}>
+          Filtrar
+        </button>
       </div>
     </div>
   );
