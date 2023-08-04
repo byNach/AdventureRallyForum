92d4c192dc93e9c3b7dcd09b4ecdee49c3766aea
nach <nachnitror@gmail.com>
2023-08-04 11:05:10 +0200

    ADD drawer (menu on mobile)

--- AdventureRallyForum/src/Components/Header.jsx
+++ AdventureRallyForum/src/Components/Header.jsx
@@ -1,11 +1,43 @@
+import { useState, useEffect, useRef } from "react";
 import "../assets/Styles/Styled-components/Header-style.css";
 import helmet from "../assets/Images/Icons/icons8-helmet-64.png";
 import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
 import { Link } from "react-router-dom";
 import { useAuth } from "../Context/AuthContext";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import { ListItemIcon } from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import GroupsIcon from '@mui/icons-material/Groups';
+import LoginIcon from '@mui/icons-material/Login';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import Divider from '@mui/material/Divider';
 
 const Header = () => {
   const auth = useAuth();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const drawerRef = useRef(null);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (drawerRef.current && !drawerRef.current.contains(event.target)) {
+        setIsDrawerOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
 
   return auth.user ? (
     <div className="Header">
@@ -54,8 +86,98 @@ const Header = () => {
           <a href="https://adventurerallyforum.flarum.cloud/" target="_blank" rel="noreferrer">
             <button className="Foro-button">FORO</button>
           </a>
-          <div className="MenuIcon-burger">
+          <div className="MenuIcon-burger" onClick={toggleDrawer}>
             <MenuIcon />
+            <Drawer
+              ref={drawerRef}
+              anchor="right"
+              open={isDrawerOpen}
+              onClose={toggleDrawer}
+              PaperProps={{
+                sx: {
+                  width: "50vw",
+                  backgroundColor: "rgba(0,0,0)",
+                  color: "rgba(255,255,255)",
+                  fontFamily: "monospace",
+                  fontSize: "1.5em",
+                },
+              }}
+            >
+              <List style={{ paddingTop: "20px" }}>
+                <ListItem>
+                  <ListItemIcon style={{ marginBottom: "10px" }}>
+                    <HomeIcon
+                      sx={{
+                        color: "white",
+                        fontSize: "3rem",
+                        marginLeft: "10px",
+                        marginRight: "20px",
+                        backgroundColor: "red",
+                      }}
+                    />
+                  </ListItemIcon>
+                  <Link to="/Home" style={{ textDecoration: "none", color: "inherit" }}>
+                    HOME
+                  </Link>
+                </ListItem>
+                <Divider style={{ borderColor: "rgba(255,255,255,0.7", borderWidth: "1px" }} />
+                <ListItem>
+                  <ListItemIcon style={{ marginBottom: "10px" }}>
+                    <GroupsIcon
+                      sx={{
+                        color: "white",
+                        fontSize: "3rem",
+                        marginLeft: "10px",
+                        marginRight: "20px",
+                        backgroundColor: "red",
+                      }}
+                    />
+                  </ListItemIcon>
+                  <a
+                    href="https://adventurerallyforum.flarum.cloud/"
+                    target="_blank"
+                    rel="noreferrer"
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    FORO
+                  </a>
+                </ListItem>
+                <Divider style={{ borderColor: "rgba(255,255,255,0.7", borderWidth: "1px" }} />
+                <ListItem>
+                  <ListItemIcon style={{ marginBottom: "10px" }}>
+                    <EventNoteIcon
+                      sx={{
+                        color: "white",
+                        fontSize: "3rem",
+                        marginLeft: "10px",
+                        marginRight: "20px",
+                        backgroundColor: "red",
+                      }}
+                    />
+                  </ListItemIcon>
+                  <Link to="/Events" style={{ textDecoration: "none", color: "inherit" }}>
+                    EVENTOS
+                  </Link>
+                </ListItem>
+                <Divider style={{ borderColor: "rgba(255,255,255,0.7", borderWidth: "1px" }} />
+                <ListItem>
+                  <ListItemIcon style={{ marginBottom: "10px" }}>
+                    <LoginIcon
+                      sx={{
+                        color: "white",
+                        fontSize: "3rem",
+                        marginLeft: "10px",
+                        marginRight: "20px",
+                        backgroundColor: "red",
+                      }}
+                    />
+                  </ListItemIcon>
+                  <Link to="/LogInPage" style={{ textDecoration: "none", color: "inherit" }}>
+                    LOGIN
+                  </Link>
+                </ListItem>
+              </List>
+            </Drawer>
           </div>
         </div>
       </div>
